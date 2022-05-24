b9b3c64db1ec736db0beb424aa44b7e8cbb19e53
Tobias <tobias.finsbol@gmail.com>
2022-05-24 13:55:24 +0200

    TobbeOS

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -15,7 +15,6 @@ else
     git clone https://aur.archlinux.org/yay-git.git ~/yay-git && cd ~/yay-git && makepkg -si && cd && rm -rf yay-git && \
     yay -S htop kitty geary gnome-keyring rofi ttf-font-awesome-4 noto-fonts-emoji xdotool dracula-gtk-theme dracula-icons-git vifm network-manager-applet paru-bin adobe-source-code-pro-fonts pacman-contrib doas xautolock nodejs-lts-fermium lxsession dmenu exa lux-git trayer yad git jre-openjdk lightdm lightdm-gtk-greeter light-locker zip feh scrot dunst pavucontrol nm-connection-editor neovim libreoffice librewolf-bin signal-desktop teams-for-linux pulseaudio picom pcmanfm emacs ripgrep && \
 
-                 rm -rf ~/.config/fish && \
                  rm -f ~/.fehbg && \
                  rm -rf ~/.config/kak && \
                  rm -rf ~/omf && \
@@ -42,8 +41,10 @@ else
                  sudo rm -rf /etc/systemd/system/sleep.target.wants && \
                  rm -rf ~/.xinitrc && \
                  rm -rf ~/.config/vifm && \
+                 rm -rf ~/Script && \
 		 rm -rf ~/.config/rofi && \
 		 rm -rf ~/.config/nvim && \
+       rm -rf ~/.emacs.d && \ 
 		 sudo rm -rf /usr/share/themes/Dracula && \
 
                  git clone https://github.com/TobbeBob123/.xmonad.git ~/.xmonad && \
@@ -67,17 +68,17 @@ else
                  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && \
                  git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d && \
-                 ~/.emacs.d/bin/doom install
-                 rm -rf ~/.doom.d
-                 git clone https://github.com/TobbeBob123/doom_emacs.git ~/.doom.d
-                 ~/.emacs.d/bin/doom sync
+                 ~/.emacs.d/bin/doom install && \
+                 rm -rf ~/.doom.d && \
+                 git clone https://github.com/TobbeBob123/doom_emacs.git ~/.doom.d && \
+                 ~/.emacs.d/bin/doom sync && \
+                 rm -rf ~/.config/fish && \
                  git clone https://github.com/TobbeBob123/Fish.git ~/.config/fish && \
                  chsh -s /usr/bin/fish && \
                  git clone https://github.com/TobbeBob123/starship.git ~/starship && \
                  mv ~/starship/starship.toml ~/.config/starship.toml && \
                  rm -rf ~/starship && \
-                 git clone https://github.com/TobbeBob123/lightdm.git 
-                 ~/lightdm && \
+                 git clone https://github.com/TobbeBob123/lightdm.git ~/lightdm && \
 
 		 sudo systemctl enable lightdm &&
                  sudo mv ~/lightdm/sleep.target.wants/ /etc/systemd/system/sleep.target.wants/ && \
