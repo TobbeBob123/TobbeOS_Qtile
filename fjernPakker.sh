5f223521ab790723ba23288ac6fcb8d5c0b58f93
Tobias <tobias.finsbol@gmail.com>
2022-05-24 09:57:23 +0200

    TobbeOS

---
+++ fjernPakker.sh
@@ -0,0 +1,1 @@
+sudo pacman -Rs eos-bash-shared eos-apps-info eos-rankmirrors eos-update-notifier reflector-simple welcome eos-log-tool
--- flereskjerm.sh
+++
@@ -1,3 +0,0 @@
-/home/tobias/.screenlayout/flere_skjerm.sh 
-feh --bg-scale Bakgrunner/wallpaper-3.jpg
-betterlockscreen -u Bakgrunner/wallpaper-5.jpg
