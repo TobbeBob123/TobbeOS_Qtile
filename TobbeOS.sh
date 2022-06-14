be61f5f5f2c8e12f2d36265f433f9fb54fe5416b
TobbeBob123 <tobiasfi@viken.no>
2022-06-14 23:26:12 +0200

    TobbeOS

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -12,7 +12,7 @@ else
 
     sudo pacman --noconfirm -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib xmobar kakoune xorg xorg-xinit fish starship lib32-mesa yay && \
     sudo rm -rf yay-git ~/yay-git && \
-    yay --noconfirm -S htop kitty discord haskell-lsp geary gnome-keyring blueman beamerpresenter-git texlive-latexextra bat ispell aspell aspell-en aspell-nb mailspring hunspell rofi ttf-font-awesome-4 noto-fonts-emoji xdotool dracula-gtk-theme dracula-icons-git vifm network-manager-applet paru-bin adobe-source-code-pro-fonts pacman-contrib doas xautolock nodejs-lts-fermium lxsession dmenu exa lux-git trayer yad git jre-openjdk lightdm lightdm-gtk-greeter light-locker zip feh scrot dunst pavucontrol nm-connection-editor neovim libreoffice librewolf-bin signal-desktop pulseaudio picom pcmanfm emacs ripgrep && \
+    yay -S htop kitty discord haskell-lsp geary gnome-keyring blueman beamerpresenter-git texlive-latexextra bat ispell aspell aspell-en aspell-nb mailspring hunspell rofi ttf-font-awesome-4 noto-fonts-emoji xdotool dracula-gtk-theme dracula-icons-git vifm network-manager-applet paru-bin adobe-source-code-pro-fonts pacman-contrib doas xautolock nodejs-lts-fermium lxsession dmenu exa lux-git trayer yad git jre-openjdk lightdm lightdm-gtk-greeter light-locker zip feh scrot dunst pavucontrol nm-connection-editor neovim libreoffice librewolf-bin signal-desktop pulseaudio picom pcmanfm emacs ripgrep && \
 
                  rm -f ~/.fehbg && \
                  rm -rf ~/.config/kak && \
@@ -46,46 +46,46 @@ else
        rm -rf ~/.emacs.d && 
 		 sudo rm -rf /usr/share/themes/Dracula && \
                  rm -rf ~/.librewolf && \
-                 rm -rf ~/org \
+                 rm -rf ~/org && \
 
 sudo systemctl enable bluetooth.service && sudo systemctl restart bluetooth.service && \
     rfkill unblock all && \
 
 mkdir ~/org && \
 
-                 git clone https://github.com/TobbeBob123/.xmonad.git ~/.xmonad && \
-                 git clone https://github.com/TobbeBob123/Script.git ~/Script && \
-                 git clone https://github.com/TobbeBob123/xmobar.git ~/.config/xmobar && \
-                 git clone https://github.com/TobbeBob123/kitty.git ~/.config/kitty && \
-                 git clone https://github.com/TobbeBob123/Bakgrunner.git ~/Bakgrunner && \
-                 git clone https://github.com/TobbeBob123/Dracula_tema.git ~/.config/gtk-3.0 && \
+                 git clone https://gitlab.com/TobbeBob123/Xmonad.git ~/.xmonad && \
+                 git clone https://gitlab.com/TobbeBob123/Script.git ~/Script && \
+                 git clone https://gitlab.com/TobbeBob123/xmobar.git ~/.config/xmobar && \
+                 git clone https://gitlab.com/TobbeBob123/kitty.git ~/.config/kitty && \
+                 git clone https://gitlab.com/TobbeBob123/Bakgrunner.git ~/Bakgrunner && \
+                 git clone https://gitlab.com/TobbeBob123/Dracula_tema.git ~/.config/gtk-3.0 && \
                  mkdir ~/.config/gtk-2.0 && \
                  cp ~/.config/gtk-3.0/settings.ini ~/.gtkrc-2.0 && \
                  cp ~/.config/gtk-3.0/settings.ini ~/.config/gtk-2.0/settings.ini && \
                  sudo mv ~/.config/gtk-3.0/gtkrc /usr/share/gtk-2.0/gtkrc && \
-                 git clone https://github.com/TobbeBob123/picom.git ~/.config/picom && \
-                 git clone https://github.com/TobbeBob123/Surf.git ~/.config/surf && \
-                 git clone https://github.com/TobbeBob123/Surf-browser.git ~/surf && \
+                 git clone https://gitlab.com/TobbeBob123/picom.git ~/.config/picom && \
+                 git clone https://gitlab.com/TobbeBob123/Surf.git ~/.config/surf && \
+                 git clone https://gitlab.com/TobbeBob123/Surf-browser.git ~/surf && \
                  cd ~/surf && sudo make install && \
 		 cd ~ && \
-                 git clone https://github.com/TobbeBob123/dunst.git ~/.config/dunst && \
-                 git clone https://github.com/TobbeBob123/kak.git ~/.config/kak && \
-                 git clone https://github.com/TobbeBob123/nvim.git ~/.config/nvim && \
+                 git clone https://gitlab.com/TobbeBob123/dunst.git ~/.config/dunst && \
+                 git clone https://gitlab.com/TobbeBob123/kak.git ~/.config/kak && \
+                 git clone https://gitlab.com/TobbeBob123/nvim.git ~/.config/nvim && \
                  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && \
                  git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d && \
                  ~/.emacs.d/bin/doom install && \
                  rm -rf ~/.doom.d && \
-                 git clone https://github.com/TobbeBob123/doom_emacs.git ~/.doom.d && \
+                 git clone https://gitlab.com/TobbeBob123/doom_emacs.git ~/.doom.d && \
                  ~/.emacs.d/bin/doom sync && \
                  rm -rf ~/.config/fish && \
-                 git clone https://github.com/TobbeBob123/starship.git ~/starship && \
+                 git clone https://gitlab.com/TobbeBob123/starship.git ~/starship && \
                  mv ~/starship/starship.toml ~/.config/starship.toml && \
                  rm -rf ~/starship && \
-                 git clone https://github.com/TobbeBob123/lightdm.git ~/lightdm && \
+                 git clone https://gitlab.com/TobbeBob123/lightdm.git ~/lightdm && \
                  sudo mv ~/.config/gtk-3.0/Dracula /usr/share/themes/Dracula && \
-                 git clone https://github.com/TobbeBob123/vifm.git ~/.config/vifm && \
-                 git clone https://github.com/TobbeBob123/Rofi.git ~/.config/rofi && \
+                 git clone https://gitlab.com/TobbeBob123/vifm.git ~/.config/vifm && \
+                 git clone https://gitlab.com/TobbeBob123/Rofi.git ~/.config/rofi && \
 
 		 sudo systemctl enable lightdm && \
                  sudo mv ~/lightdm/sleep.target.wants/ /etc/systemd/system/sleep.target.wants/ && \
@@ -96,7 +96,7 @@ mkdir ~/org && \
                  touch ~/.xinitrc && echo exec xmonad > ~/.xinitrc && \
                  xmonad --recompile && \
 
-                 git clone https://github.com/TobbeBob123/Fish.git ~/.config/fish && \
+                 git clone https://gitlab.com/TobbeBob123/Fish.git ~/.config/fish && \
                  chsh -s /usr/bin/fish && \
                  curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
