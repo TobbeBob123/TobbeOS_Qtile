61ee3e426ff99596bc47ad80dea520a6cd869dd9
Tobias <tobiasfi@viken.no>
2022-09-24 13:32:37 +0200

    TobbeOS

--- TobbeOS.sh
+++ TobbeOS.sh
@@ -15,7 +15,7 @@ else
     sudo pacman --noconfirm -Sy && sudo pacman -Syu && sudo pacman -S xmonad xmonad-contrib xmobar kakoune xorg xorg-xinit fish starship lib32-mesa && \
     sudo rm -rf yay-git ~/yay-git && \
     git clone https://aur.archlinux.org/yay-git.git ~/yay-git && cd ~/yay-git && makepkg -si && \
-    yay -S htop kitty base-devel vim discord haskell-lsp geary gnome-keyring blueman beamerpresenter-git texlive-latexextra bat ispell aspell aspell-en aspell-nb mailspring hunspell rofi ttf-font-awesome-4 noto-fonts-emoji xdotool dracula-gtk-theme dracula-icons-git vifm network-manager-applet paru-bin adobe-source-code-pro-fonts pacman-contrib doas xautolock nodejs-lts-fermium lxsession dmenu exa lux-git trayer yad git jre-openjdk lightdm lightdm-gtk-greeter light-locker zip feh scrot dunst pavucontrol nm-connection-editor neovim libreoffice librewolf-bin signal-desktop pulseaudio pulseaudio-bluetooth picom pcmanfm emacs ripgrep lxappearance qt5ct dracula-cursors-git && \
+    yay -S htop kitty base-devel vim discord haskell-lsp geary gnome-keyring blueman flameshot beamerpresenter-git texlive-latexextra bat ispell aspell aspell-en aspell-nb mailspring hunspell rofi ttf-font-awesome-4 noto-fonts-emoji xdotool dracula-gtk-theme dracula-icons-git vifm network-manager-applet paru-bin adobe-source-code-pro-fonts pacman-contrib doas xautolock nodejs-lts-fermium lxsession dmenu exa lux-git trayer yad git jre-openjdk lightdm lightdm-gtk-greeter light-locker zip feh scrot dunst pavucontrol nm-connection-editor neovim libreoffice librewolf-bin signal-desktop pulseaudio pulseaudio-bluetooth picom pcmanfm emacs ripgrep lxappearance qt5ct dracula-cursors-git && \
 
 rm -rf ~/Pictures && \
 mkdir ~/Pictures && \
